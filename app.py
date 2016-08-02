0030c6193db2963040290abbd2c6623a83fff423
Roshan Prabhakar <roshancvp@gmail.com>
2016-08-01 20:10:15 -0700

    Added Rides and Textbooks

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, url_for, redirect
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from db_setup import Base, Sell
+from db_setup import Base, Sell, Ride, Textbook
 
 app = Flask(__name__)
 
@@ -17,18 +17,40 @@ def landing():
 
 @app.route('/dashboard/')
 def dashboard():
-    items = session.query(Sell)
-    return render_template('dashboard.html', items=items)
+    sell_items = session.query(Sell)
+    ride_items = session.query(Ride)
+    textbook_items = session.query(Textbook)
+    return render_template('dashboard.html', sell_items = sell_items, ride_items=ride_items, textbook_items=textbook_items)
 
 @app.route('/dashboard/sell/new', methods=['GET', 'POST'])
 def sell_new():
     if request.method == 'POST':
-        new_item = Sell(item=request.form['item'], desc=request.form['desc'], price=request.form['price'])
-        session.add(new_item)
+        sell_item = Sell(item=request.form['item'], desc=request.form['desc'], price=request.form['price'])
+        session.add(sell_item)
         session.commit()
         return redirect(url_for('dashboard'))
     else:
         return render_template('sell_new.html')
+
+@app.route('/dashboard/ride/new', methods=['GET', 'POST'])
+def ride_new():
+    if request.method == 'POST':
+        ride_item = Ride(origin=request.form['origin'], destination=request.form['destination'], price=request.form['price'])
+        session.add(ride_item)
+        session.commit()
+        return redirect(url_for('dashboard'))
+    else:
+        return render_template('ride_new.html')
+
+@app.route('/dashboard/textbook/new', methods=['GET', 'POST'])
+def textbook_new():
+    if request.method == 'POST':
+        textbook_item = Textbook(lecture=request.form['lecture'], textbook=request.form['textbook'], price=request.form['price'])
+        session.add(textbook_item)
+        session.commit()
+        return redirect(url_for('dashboard'))
+    else:
+        return render_template('textbook_new.html')
 
 if __name__ == '__main__':
     app.debug = True
--- db_setup.py
+++ db_setup.py
@@ -13,7 +13,23 @@ class Sell(Base):
     sell_id = Column(Integer, primary_key=True)
     item = Column(String(50), nullable=False)
     desc = Column(String(50))
-    price = Column(Float)
+    price = Column(Float, nullable=False)
+
+class Ride(Base):
+    __tablename__ = 'ride'
+
+    sell_id = Column(Integer, primary_key=True)
+    origin = Column(String(50), nullable=False)
+    destination = Column(String(50), nullable=False)
+    price = Column(Float, nullable=False)
+
+class Textbook(Base):
+    __tablename__ = 'textbook'
+
+    sell_id = Column(Integer, primary_key=True)
+    lecture = Column(String(50), nullable=False)
+    textbook = Column(String(50), nullable=False)
+    price = Column(Float, nullable=False)
 
 engine = create_engine('sqlite:///bulletin.db')
 
